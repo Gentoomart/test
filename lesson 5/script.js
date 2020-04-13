2106eaf20538d35d55f6bb623f12c7e8a85fea3d
Dmytro <gentoo.mart@gmail.com>
2020-04-13 12:54:33 +0300

    Это первый коммит

---
+++ lesson 5/script.js
@@ -0,0 +1,34 @@
+/* 'use strict';
+ */
+a = 5;
+console.log(a);
+
+/* let arr = ["1.png", "2.jpg", "3.bmp"] */
+
+/* 
+alert('Hello World'); */
+
+/* let answer = confirm('Are you here?');
+console.log(answer); */
+
+/* let answer = +prompt('Есть ли Вам 18?', "Дав");
+
+console.log(typeof(answer));
+
+console.log("arr" + " - object");
+console.log(4 + +" - object"); */
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log('2' === 2);
+
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);
